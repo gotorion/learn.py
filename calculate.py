b91c614e7197306980774bbedef81885a7027d01
Junhui Li <junhui.lee@icloud.com>
2025-07-03 09:19:28 +0800

    Add new file

---
+++ calculate.py
@@ -0,0 +1,13 @@
+import math
+
+print(math.sin(90))
+print(math.factorial(3))
+print(math.log(1))
+
+# def a function
+def add(num1, num2):
+    return num1 + num2
+
+print(add(1, 2))
+print(add(1.0, 2))
+print(add("1", "2"))
