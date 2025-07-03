409378e2f0d09b45dcff8e61993d381be05f0619
Junhui Li <junhui.lee@icloud.com>
2025-07-03 09:48:52 +0800

    Add new file

---
+++ list.py
@@ -0,0 +1,13 @@
+# list can store any type
+things_i_want_buy = ["Computer", "iPhone", "Camera"]
+print(things_i_want_buy)
+
+things_i_want_buy.append("Monitor")
+print(things_i_want_buy)
+
+things_i_want_buy.remove("iPhone")
+print(things_i_want_buy)
+print(sorted(things_i_want_buy))
+
+things_i_want_buy.append(1)
+print(things_i_want_buy)
