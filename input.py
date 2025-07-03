03a3d86864914bf1758afa50abb65a8bd0360c52
Junhui Li <junhui.lee@icloud.com>
2025-07-03 09:31:19 +0800

    Add new file

---
+++ input.py
@@ -0,0 +1,4 @@
+user_weight = float(input("Enter your weight: "))
+user_height = input("Enter your height: ")
+
+print("BMI = " + str(user_weight / (float(user_height) ** 2)))
---
+++ type.py
@@ -0,0 +1,11 @@
+var = None
+print(type(var))
+
+boolean_var = False
+print(type(boolean_var))
+
+s = "Hello"
+print(len(s))
+print(type(s))
+print(s[0])
+print(s[len(s) - 1])
