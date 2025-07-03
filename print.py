c3d7539b8a32d7b2df42c886a75c9b5f6e83393b
Junhui Li <junhui.lee@icloud.com>
2025-07-03 09:08:53 +0800

    Add project

---
+++ print.py
@@ -0,0 +1,10 @@
+print("Hello!!!")
+print("This is a \'string test\'")
+print("Connect" + " different " + "words")
+print("""This
+is
+multi
+line""")
+
+phone_number = "12345"
+print("Phone number = " + phone_number)
