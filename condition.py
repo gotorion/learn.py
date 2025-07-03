19210bfb740af06d5c635924e77f21de7411d25d
Junhui Li <junhui.lee@icloud.com>
2025-07-03 09:43:08 +0800

    Add new file

---
+++ condition.py
@@ -0,0 +1,11 @@
+if True:
+    print(True)
+    num = int(input("Enter a number: "))
+    if num <= 60:
+        print("Ok")
+    elif num <= 80:
+        print("Fine")
+    else:
+        print("Not Ok")
+else:
+    print(False)
