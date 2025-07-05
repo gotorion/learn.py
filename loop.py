8fe25ff667f21675534aa5bae8609cab6e1311de
Junhui Li <junhui.lee@icloud.com>
2025-07-05 13:38:14 +0800

    Add new file

---
+++ loop.py
@@ -0,0 +1,12 @@
+nums = [1, 2, 3, 4, 5, 6]
+
+for num in nums:
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+count = 10
+while count >= 0:
+    print(count)
+    count = count - 1
